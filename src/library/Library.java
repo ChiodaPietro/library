404140c9a3470b3235d6910d7ebe82c4d66b00e9
ChiodaPietro <chioda.pietro@studenti.salesianisesto.it>
2022-10-03 10:00:57 +0200

    coded the first classes, library and book_types

---
+++ src/library/Library.java
@@ -0,0 +1,13 @@
+package library;
+
+public class Library {
+    private String name;
+    private String address;
+    private int phone_number;
+
+    public Library(String name, String address, int phone_number){
+        this.name=name;
+        this.address=address;
+        this.phone_number=phone_number;
+    }
+}
--- src/library/main.java
+++ src/library/main.java
@@ -2,6 +2,11 @@ package library;
 
 public class main {
     public static void main(String[] args) {
-
+        Library library= new Library("name", "address", 222222222);
+        book_types book_types= new book_types("thriller","yellow","fantasy", "science fiction", "romance", "science" );
+        book_types[] list= new book_types[6];
+        for(int i = 0 ; i < list.length; i++){
+            System.out.println(list[4]);
+        }
     }
 }
